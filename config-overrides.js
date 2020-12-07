2044e5a8ac3849a62d3a01d40971ad20fdb14db2
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-12-07 18:12:31 +0100

    Made a change

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,10 @@
-module.exports = {
-	devServer: {
-		disableHostCheck: true,
-	},
-};
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    devServer: {
+        compress: true,
+        disableHostCheck: true,   // That solved it
+    
+     }
+    return config;
+  }
+  
